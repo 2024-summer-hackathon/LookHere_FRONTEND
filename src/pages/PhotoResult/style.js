01645dd2c0a9cc70e5c5dbbde1cc24d24bd1e4c8
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-17 17:35:49 +0900

    위치 조정

--- src/pages/PhotoResult/style.js
+++ src/pages/PhotoResult/style.js
@@ -4,34 +4,33 @@ export const Layout = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding-top: 130px;
+  padding-top: 80px;
   width: 100%;
 `;
 
 export const Frame = styled.div`
   position: relative;
-  /* top: 30px; */
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 189px;
-  height: 579px;
+  width: 250px;
+  height: 766px;
   background-image: url(${(props) => props.frameUrl});
   background-size: cover;
   background-position: center;
-  padding-top: 19px; /* 사진을 아래로 내리기 위한 패딩 추가 */
-  gap: 16px; /* 이미지 간의 간격 */
+  padding-top: 19px;
+  gap: 10px;
 `;
 
 export const ImageWrapper = styled.div`
-  width: 153px;
-  height: 105px;
+  width: 199px;
+  height: 150px;
   overflow: hidden;
   z-index: -1;
 
   img {
     width: 100%;
     height: 100%;
-    object-fit: cover; /*이미지가 박스를 채우도록 조정 */
+    object-fit: cover;
   }
 `;
