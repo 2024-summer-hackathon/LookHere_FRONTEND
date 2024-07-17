4e9c0b26f2512db746a10ee3a709eb4a4053d7f5
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-17 17:12:18 +0900

    프레임 적용

--- src/pages/ChooseFrame/index.jsx
+++ src/pages/ChooseFrame/index.jsx
@@ -8,14 +8,17 @@ import * as t from "./style.js";
 const App = () => {
   const [selected, setSelected] = useState(null);
   const navigate = useNavigate();
-  const handleSelect = (index) => {
-    setSelected(index);
+
+  const handleSelect = (id) => {
+    setSelected(id);
+    localStorage.setItem("selectedFrameId", id); // Save the selected frame id to localStorage
+    navigate("/camera");
   };
 
   const items = [
-    {id: 1, src: Loopy, frameColor: "pink", label: "루피 프레임" },
-    {id:2, src: Soul, frameColor: "red", label: "정소울 프레임" },
-    {id:3, src: Ari, frameColor: "yellow", label: "아리소리 프레임" },
+    { id: 1, src: Loopy, frameColor: "pink", label: "루피 프레임" },
+    { id: 2, src: Soul, frameColor: "red", label: "정소울 프레임" },
+    { id: 3, src: Ari, frameColor: "yellow", label: "아리소리 프레임" },
   ];
 
   return (
@@ -27,15 +30,11 @@ const App = () => {
         </t.Desc>
       </t.Topbar>
       <t.Container>
-        {items.map((item, index) => (
+        {items.map((item) => (
           <t.ItemContainer
-            key={index}
-            selected={selected === index}
-            onClick={() => {
-              handleSelect(index);
-              localStorage.setItem("frameIndex", index);
-              navigate("/camera");
-            }}
+            key={item.id}
+            selected={selected === item.id}
+            onClick={() => handleSelect(item.id)}
           >
             <t.Item src={item.src} />
             <t.Tag frameColor={item.frameColor}>{item.label}</t.Tag>
--- src/pages/PhotoResult/index.jsx
+++ src/pages/PhotoResult/index.jsx
@@ -1,16 +1,25 @@
+import React, { useEffect, useState } from "react";
 import ruppeFrame from "../../assets/rupeeFrame.png";
 import soulFrame from "../../assets/soulFrame.png";
 import alrisoriFrame from "../../assets/alrisoriFrame.png";
 import * as S from "./style";
 
-export default function PhotoResult({ id, imgs }) {
+export default function PhotoResult({ imgs }) {
   const frames = [
     { id: 1, url: ruppeFrame },
     { id: 2, url: soulFrame },
     { id: 3, url: alrisoriFrame },
   ];
 
-  const selectedFrame = frames.find((frame) => frame.id === id);
+  const [selectedFrame, setSelectedFrame] = useState(null);
+
+  useEffect(() => {
+    const frameId = localStorage.getItem("selectedFrameId");
+    if (frameId) {
+      const frame = frames.find((frame) => frame.id === parseInt(frameId));
+      setSelectedFrame(frame);
+    }
+  }, []);
 
   return (
     <S.Layout>
