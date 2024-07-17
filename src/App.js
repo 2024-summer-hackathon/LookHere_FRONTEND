311e9093a3f5d8b6e33587bac7acaaa6e8ae0434
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-17 14:47:36 +0900

    경로 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ChooseKeyWord from "./pages/ChooseKeyWord";
+import ChooseFrame from "./pages/ChooseFrame";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ChooseKeyWord />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/choose-keyword" element={<ChooseKeyWord />} />
+      <Route path="/choose-frame" element={<ChooseFrame />} />
     </Routes>
   );
 }
